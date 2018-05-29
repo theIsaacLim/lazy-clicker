f6cb03ade4d9c6a3b66430614377a7a5aa2cce5f
Isaac Lim <33272667+theIsaacLim@users.noreply.github.com>
2018-05-29 16:18:47 +0800

    4 lines!
    Could compress it more but that would kill readability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
-from time import sleep
 driver = webdriver.Chrome("./chromedriver")
 driver.get("http://orteil.dashnet.org/cookieclicker/")
-sleep(5)
+driver.implicitly_wait(5)
 while True: driver.find_element_by_id('bigCookie').click()
