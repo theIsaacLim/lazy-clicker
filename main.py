cefc4cf995c9200c1ae78f1d670eab91db87f557
Isaac Lim <33272667+theIsaacLim@users.noreply.github.com>
2018-06-01 10:25:03 +0800

    6 lines but twice as fast

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from selenium import webdriver
 driver = webdriver.Chrome("./chromedriver")
 driver.get("http://orteil.dashnet.org/cookieclicker/")
 driver.implicitly_wait(5)
-while True: driver.find_element_by_id('bigCookie').click()
+c = driver.find_element_by_id('bigCookie')
+while True: c.click()
